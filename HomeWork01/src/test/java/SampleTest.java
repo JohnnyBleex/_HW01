c80ad2bc4832436c6bbb20a8d52378e2ba5dc1a9
Sergey <19ser98@mail.ru>
2021-08-06 02:13:56 +0300

    Сделал переход на страницу "смартфоны", сделал скриншот страницы

--- HomeWork01/src/test/java/SampleTest.java
+++ HomeWork01/src/test/java/SampleTest.java
@@ -3,11 +3,17 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Sleeper;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 
@@ -54,6 +60,36 @@ public class SampleTest {
         GettingCookies.getCookieOutput(driver);
 
         waitingForAPage(10);
+    }
+
+    @Test
+    public void openPageDnsAndFinedSmartPhone(){
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+
+        driver.get(dnsAddress);
+        logger.info("Открыта страница DNS - " + dnsAddress);
+
+        WebElement elementChooseACity = driver.findElement(By.xpath("//a[@class='btn btn-additional']"));
+        elementChooseACity.click();
+
+        WebElement smartphonesAndGadgets = driver.findElement(By.xpath("//div/a[text()='Смартфоны и гаджеты']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(smartphonesAndGadgets).perform();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+        By smartphonesXpath = By.xpath("//div/a[text()='Смартфоны']");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(smartphonesXpath));
+
+        WebElement smartphones = driver.findElement(smartphonesXpath);
+        smartphones.click();
+
+        try {
+            Screenshot screenshot = new AShot().takeScreenshot(driver);
+            ImageIO.write(screenshot.getImage(), "png", new File("screens\\ASViewablePage.png"));
+            logger.info("Скриншот сохранен в папку screens");
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
     }
 
     @AfterEach
